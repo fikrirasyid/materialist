370acbbc30adae8f642bf19775be3a692f4f4268
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-16 18:26:51 +0700

    Improving civil-footnotes UX
    - Slide instead of jump when clicking footnotes
    - Better link styling

--- js/materialist.js
+++ js/materialist.js
@@ -59,6 +59,19 @@ jQuery(document).ready(function($) {
 		$('body').toggleClass( target_id + '-expanded' );
 	});
 
+	/**
+	* Civil Footnotes Support
+	* Slide the window instead of jumping it
+	*/
+	$('#main').on( 'click', 'a[rel="footnote"], a.backlink', function(e){
+		e.preventDefault();
+		var target_id = $(this).attr('href');
+		var respond_offset = $(target_id).offset();
+
+		$('html, body').animate({
+			scrollTop : respond_offset.top - 100
+		});
+	});	
 });
 
 /**
